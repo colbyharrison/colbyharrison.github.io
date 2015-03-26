f52e2de213cedad2db21ecb363060cbe43d67780
Colby Harrison <ceh011@ucsd.edu>
2015-03-26 09:07:31 -0700

    Changed the way  to display different blocks, now hiding and showing blocks, using display 'none' and 'block'

--- background.js
+++ background.js
@@ -2,40 +2,54 @@
 function runScript(e) {
 	//var console = document.getElementById('console');
     	if (e.keyCode == 13) { 
-            var help ="<br><p style='float:left;margin-right: 75px;''>Hello</p><p style='float: left;''>ABM</p>";
-    		var para = document.createElement('div');
-    		para.setAttribute('id','replace');
+
+           // console.log(document.getElementById('container'));
+    		// var para = document.createElement('div');
+    		// para.setAttribute('id','replace');
     		var terminal = document.getElementById('console');
     		var commandLine = document.getElementById('command');
     		var input = commandLine.value;
 
+            var toPrint = document.getElementsByClassName("printed");
+            //Any thing that could be displayed is closed. The reason I do 
+            //this is because global variables are bad. So this is a simple fix.
+            //Just close every element then display them when requested.
+            for (i = 0; i < toPrint.length; i++) {
+                toPrint[i].style.display = 'none';
+            }
+
     		switch(input){
     			case "resume":
-    				para.innerHTML= "$Input..."+ input+
-    				"Resume Coming";
+    				// para.innerHTML= "$Input..."+ input+"Resume Coming";
+                    document.getElementById('resume').style.display = 'block';
     				break;
     			case "help":
     				//para.setAttribute('id','help');
     				//terminal.insertBefore(para,document.getElementById('output'));
     				//$('#help').load('help.html');
-    				para.innerHTML= "$Input..."+ input + help;
+    				// para.innerHTML= "$Input..."+ input + "<br> Help ME";
+                    document.getElementById('help').style.display = 'block';
     				break;
     			case "about":
-    				para.innerHTML= "$Input..."+ input + "<br>About goes here";
+                    document.getElementById('about').style.display = 'block';
+    				// para.innerHTML= "$Input..."+ input + "<br>About goes here";
     				break;
     			case "contact":
-    				para.innerHTML= "$Input..."+ input + "<br>Contact goes here";
+                    document.getElementById('contact').style.display = 'block';
+    				// para.innerHTML= "$Input..."+ input + "<br>Contact goes here";
     				break;
     			case "projects":
-    				para.innerHTML= "$Input..."+ input + "<br>Projects goes here";
+                    document.getElementById('projects').style.display = 'block';
+    				// para.innerHTML= "$Input..."+ input + "<br>Projects goes here";
     				break;
     			default:
-    				para.innerHTML= "$Input..."+ input + "<br>No Such Command <br> You need: help";
+                    document.getElementById('error').style.display = 'block';
+    				// para.innerHTML= "$Input..."+ input + "<br>No Such Command <br> You need: help";
     		}
 
 
     		//terminal.insertBefore(para,document.getElementById('output'));
-    		terminal.replaceChild(para, document.getElementById('replace'))
+    		// terminal.replaceChild(para, document.getElementById('replace'))
     		document.getElementById('command').value = '';
     	}
     }
